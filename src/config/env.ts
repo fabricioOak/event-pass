fb9fd16d48797d28b75ad311d1d4cf15d4305c07
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-09-22 14:27:23 -0300

    env validation

---
+++ src/config/env.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().startsWith("postgresql://"),
+  JWT_SECRET: z.string(),
+  COOKIE_SECRET: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
