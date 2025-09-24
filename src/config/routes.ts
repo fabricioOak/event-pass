c8593b12ea668f4755d142c1728bdf4274cc4dc0
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-09-23 21:49:08 -0300

    update routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import { userRoutes } from "../modules/users/routes/user.routes.ts";
+import { authRoutes } from "../modules/auth/routes/auth.routes.ts";
 
 export const routes = (app: FastifyInstance) => {
   app.register(userRoutes, {
     prefix: "/users",
+  });
+  app.register(authRoutes, {
+    prefix: "/auth",
   });
 };
