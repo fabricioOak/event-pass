5ebc4c1f99f345c4cf2841e7d83ec7a200d38753
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-09-23 21:49:37 -0300

    fix user controller interface

--- src/shared/interfaces/users/UserController.ts
+++ src/shared/interfaces/users/UserController.ts
@@ -1,38 +1,13 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
-import type { CreateUserInput } from "../../../modules/users/schemas/create.schema.ts";
-import type { UpdateUserInput } from "../../../modules/users/schemas/update.schema.ts";
-import type { PaginationQuery } from "../../../modules/users/schemas/pagination.schema.ts";
 
 export interface IUserController {
-  createUser(
-    request: FastifyRequest<{
-      Body: CreateUserInput;
-    }>,
-    reply: FastifyReply
-  ): Promise<void>;
-  findManyUsers(
-    request: FastifyRequest<{
-      Querystring: PaginationQuery;
-    }>,
-    reply: FastifyReply
-  ): Promise<void>;
-  findUserById(
-    request: FastifyRequest<{
-      Params: { id: string };
-    }>,
-    reply: FastifyReply
-  ): Promise<void>;
-  updateUser(
-    request: FastifyRequest<{
-      Body: UpdateUserInput;
-      Params: { id: string };
-    }>,
-    reply: FastifyReply
-  ): Promise<void>;
-  deleteUser(
-    request: FastifyRequest<{
-      Params: { id: string };
-    }>,
-    reply: FastifyReply
-  ): Promise<void>;
+  createUser(request: FastifyRequest, reply: FastifyReply): Promise<void>;
+
+  findManyUsers(request: FastifyRequest, reply: FastifyReply): Promise<void>;
+
+  findUserById(request: FastifyRequest, reply: FastifyReply): Promise<void>;
+
+  updateUser(request: FastifyRequest, reply: FastifyReply): Promise<void>;
+
+  deleteUser(request: FastifyRequest, reply: FastifyReply): Promise<void>;
 }
